4d565ece4617969fe0b7b7bcca68ae454a1f8954
Joott <jonathan.ott1994@gmail.com>
2021-11-24 22:21:28 +0100

    Add additional chart options

--- src/BioFSharp.Vis/Upset.fs
+++ src/BioFSharp.Vis/Upset.fs
@@ -6,6 +6,13 @@ module Upset =
     open Plotly.NET
     open Plotly.NET.LayoutObjects
     
+    let combineArrayAlternating (a: array<'a>) (b: array<'a>) =
+        List.fold2 (fun acc a' b' ->
+            b'::a'::acc   
+        ) [] (a |> List.ofArray) (b |> List.ofArray)
+        |> List.rev
+        |> Array.ofList
+
     let createLinearAxisWithRange (maxRange: float) =
         let range  = StyleParam.Range.MinMax (-0.5,maxRange)
         LinearAxis.init(Range=range, ShowGrid=false, ShowLine=false, ShowTickLabels=false, ZeroLine=false)
@@ -39,11 +46,10 @@ module Upset =
             Color = color
         )
 
-    let createIntersectionPlotPart (position:int) (intersectingSets: string) (labelIDs: (string*int)[]) (markerSize: int) (colorIntersecting: Color) (colorNotIntersecting: Color) =
+    let createIntersectionPlotPart (position:int) (intersectingSets: string list) (labelIDs: (string*int)[]) (markerSize: int) (colorIntersecting: Color) (colorNotIntersecting: Color) =
         let setIDsPresent, setIDsNotPresent =
-            let intersectingSetsArray = intersectingSets.Split '&'
             labelIDs
-            |> Array.partition (fun (label,_) -> intersectingSetsArray |> Array.contains label)
+            |> Array.partition (fun (label,_) -> intersectingSets |> List.contains label)
         let lineChart =
             let idWithPosition =
                 setIDsPresent
@@ -77,8 +83,8 @@ module Upset =
         |> Chart.withXAxisStyle("Set Size",MinMax=(float maxSetSize,0.), Domain=domainSet, TitleFont=textFont)
         |> Chart.withYAxis (createLinearAxisWithRange maxY)
         |> Chart.withTraceName(ShowLegend=false)
-    Layout.init
-    let createIntersectionSizePlots (intersectionCount: (string*int)[]) (maxX: float) (color: Color) (domainIntersection: float*float) (textFont: Font) =
+
+    let createIntersectionSizePlots (intersectionCount: (string list*int)[]) (maxX: float) (color: Color) (domainIntersection: float*float) (textFont: Font) =
         intersectionCount
         |> Array.map snd
         |> fun count -> 
@@ -87,18 +93,60 @@ module Upset =
         |> Chart.withYAxisStyle("Intersection Size", TitleFont=textFont)
         |> Chart.withTraceName(ShowLegend=false)
 
-    let createUpsetWith (labels:array<string>) (sets:array<Set<'a>>) (markerSize: int) (mainColor: Color) (secondaryColor: Color) (domainSet: float*float) (domainIntersection: float*float) (textFont: Font) =
+    let alignIntersectionData (venn: (string list * Set<'a>)[]) (setData: Map<'a,'b>) =
+        venn
+        |> Array.map (fun (_,set) ->
+            set
+            |> Set.toArray
+            |> Array.map (fun entry ->
+                setData.[entry]
+            )
+        )
+
+    let createIntersectionDataPlots (intersectionData: array<array<'b>>) (title: string) (maxX: float) (chartFun: array<'b> -> GenericChart.GenericChart) (domainIntersection: float*float) (textFont: Font) =
+        intersectionData
+        |> Array.map chartFun
+        |> Chart.combine
+        |> Chart.withLegend false
+        |> Chart.withXAxis (createLinearAxisWithRangeDomain maxX domainIntersection)
+        |> Chart.withYAxisStyle (title, TitleFont=textFont)
+
+    let createUpsetWith (labels:array<string>) (sets:array<Set<'a>>) (setData: array<Map<'a,'b>>) (setDataChartsTitle: array<(array<'b> -> GenericChart.GenericChart)*string>) 
+        (markerSize: int) (mainColor: Color) (secondaryColor: Color) (domainSet: float*float) (domainIntersection: float*float) (textFont: Font) (maxIntersections: int) =
         let labelIDs =
             labels
             |> Array.mapi (fun i label -> label,i)
+        let venn =
+            Venn.ofSetList labels sets|> Map.toArray
+            |> Array.map (fun (_,labelSet) -> 
+                labelSet.Label, labelSet.Set
+            )
+            |> Array.filter (fun (id,_) -> not id.IsEmpty)
+            |> Array.sortByDescending (snd >> Set.count)
+            |> fun v ->
+                if v.Length <= maxIntersections then
+                    v
+                else
+                    v
+                    |> Array.take maxIntersections
         let vennCount =
-            Venn.ofSetList labels sets
-            |> Venn.toVennCount
-            |> Map.toArray
-            |> Array.filter (fun (id,_) -> id <> "union")
-            |> Array.sortByDescending snd
+            venn
+            |> Array.map (fun (id,set) -> id,set.Count)
         let maxX = float vennCount.Length - 0.5
         let maxY = float labels.Length - 0.5
+        let intersectionData =
+            setData
+            |> Array.map (alignIntersectionData venn)
+        let intersectionDataCharts =
+            let charts =
+                intersectionData
+                |> Array.map2 (fun (chartFun,title) sD ->
+                    createIntersectionDataPlots sD title maxX chartFun domainIntersection textFont
+                ) setDataChartsTitle
+            let emptyCharts = 
+                [|0 .. setData.Length - 1|]
+                |> Array.map (fun _ -> Chart.Invisible())
+            combineArrayAlternating emptyCharts charts
         let intersectionPlot =
             vennCount
             |> Array.mapi (fun position (intersectingSets,_) ->
@@ -111,14 +159,16 @@ module Upset =
         let setSizePlot = createSetSizePlot labels sets maxY mainColor domainSet textFont
         let intersectionSizePlot = createIntersectionSizePlots vennCount maxX mainColor domainIntersection textFont
         let grid =
-            [
-                Chart.Invisible()
-                intersectionSizePlot
-                setSizePlot
-                intersectionPlot
-            ]
-            |> Chart.Grid(2,2)
+            Array.append
+                intersectionDataCharts
+                [|
+                    Chart.Invisible()
+                    intersectionSizePlot
+                    setSizePlot
+                    intersectionPlot
+                |]
+            |> Chart.Grid(2+setData.Length,2)
         grid
 
-    let createUpset (labels:array<string>) (sets:array<Set<'a>>) =
-        createUpsetWith labels sets 15 Color.Table.Office.darkBlue Color.Table.Office.lightBlue (0.,0.2) (0.3,1.) (Font.init(StyleParam.FontFamily.Arial,Size=20.))
+    let createUpset (labels: array<string>) (sets: array<Set<'a>>) =
+        createUpsetWith labels sets [||] [||] 15 Color.Table.Office.darkBlue Color.Table.Office.lightBlue (0., 0.2) (0.3, 1.) (Font.init(StyleParam.FontFamily.Arial, Size=20.)) 20
