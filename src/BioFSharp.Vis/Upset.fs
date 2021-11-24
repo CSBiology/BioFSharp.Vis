06d85f9cf6d98c66878aab013e74baa977016812
Joott <jonathan.ott1994@gmail.com>
2021-11-24 19:50:06 +0100

    Add font option

--- src/BioFSharp.Vis/Upset.fs
+++ src/BioFSharp.Vis/Upset.fs
@@ -14,9 +14,9 @@ module Upset =
         let range  = StyleParam.Range.MinMax (-0.5,maxRange)
         LinearAxis.init(Range=range, ShowGrid=false, ShowLine=false, ShowTickLabels= false, ZeroLine=false, Domain=StyleParam.Range.MinMax domain)
 
-    let createLinearAxisWithRangeTickLabel (maxRange: float) (labels: string[]) =
+    let createLinearAxisWithRangeTickLabel (maxRange: float) (labels: string[]) (font: Font) =
         let range  = StyleParam.Range.MinMax (-0.5,maxRange)
-        LinearAxis.init(Range=range, ShowGrid=false, ShowLine=false, ShowTickLabels=true, ZeroLine=false, TickMode=StyleParam.TickMode.Array, TickVals=[0 .. labels.Length - 1], TickText=labels)
+        LinearAxis.init(Range=range, ShowGrid=false, ShowLine=false, ShowTickLabels=true, ZeroLine=false, TickMode=StyleParam.TickMode.Array, TickVals=[0 .. labels.Length - 1], TickText=labels, TickFont=font)
 
     let createIntersectionLineChart (data: (int*int)[]) (markerSize: int) (color: Color) =
         Chart.Line (
@@ -64,7 +64,7 @@ module Upset =
         ]
         |> Chart.combine
 
-    let createSetSizePlot (labels:array<string>) (sets:array<Set<'a>>) (maxY: float) (color: Color) (domainSet: float*float) =
+    let createSetSizePlot (labels:array<string>) (sets:array<Set<'a>>) (maxY: float) (color: Color) (domainSet: float*float) (textFont: Font) =
         let labelCount =
             Array.map2 (fun label (set: Set<'a>) ->
                 label, set.Count
@@ -74,20 +74,20 @@ module Upset =
             |> Array.maxBy snd
             |> snd
         Chart.Bar (labelCount, Color = color)
-        |> Chart.withXAxisStyle("Set Size",MinMax = (float maxSetSize,0.), Domain = domainSet)
+        |> Chart.withXAxisStyle("Set Size",MinMax=(float maxSetSize,0.), Domain=domainSet, TitleFont=textFont)
         |> Chart.withYAxis (createLinearAxisWithRange maxY)
         |> Chart.withTraceName(ShowLegend=false)
-
-    let createIntersectionSizePlots (intersectionCount: (string*int)[]) (maxX: float) (color: Color) (domainIntersection: float*float) =
+    Layout.init
+    let createIntersectionSizePlots (intersectionCount: (string*int)[]) (maxX: float) (color: Color) (domainIntersection: float*float) (textFont: Font) =
         intersectionCount
         |> Array.map snd
         |> fun count -> 
             Chart.Column(count, Color = color)
         |> Chart.withXAxis (createLinearAxisWithRangeDomain maxX domainIntersection)
-        |> Chart.withYAxisStyle("Intersection Size")
+        |> Chart.withYAxisStyle("Intersection Size", TitleFont=textFont)
         |> Chart.withTraceName(ShowLegend=false)
 
-    let createUpsetWith (labels:array<string>) (sets:array<Set<'a>>) (markerSize: int) (mainColor: Color) (secondaryColor: Color) (domainSet: float*float) (domainIntersection: float*float) =
+    let createUpsetWith (labels:array<string>) (sets:array<Set<'a>>) (markerSize: int) (mainColor: Color) (secondaryColor: Color) (domainSet: float*float) (domainIntersection: float*float) (textFont: Font) =
         let labelIDs =
             labels
             |> Array.mapi (fun i label -> label,i)
@@ -105,11 +105,11 @@ module Upset =
                 createIntersectionPlotPart position intersectingSets labelIDs markerSize mainColor secondaryColor
             )
             |> Chart.combine
-            |> Chart.withYAxis (createLinearAxisWithRangeTickLabel maxY labels)
+            |> Chart.withYAxis (createLinearAxisWithRangeTickLabel maxY labels textFont)
             |> Chart.withXAxis (createLinearAxisWithRangeDomain maxX domainIntersection)
             |> Chart.withTraceName(ShowLegend=false)
-        let setSizePlot = createSetSizePlot labels sets maxY mainColor domainSet
-        let intersectionSizePlot = createIntersectionSizePlots vennCount maxX mainColor domainIntersection
+        let setSizePlot = createSetSizePlot labels sets maxY mainColor domainSet textFont
+        let intersectionSizePlot = createIntersectionSizePlots vennCount maxX mainColor domainIntersection textFont
         let grid =
             [
                 Chart.Invisible()
@@ -121,4 +121,4 @@ module Upset =
         grid
 
     let createUpset (labels:array<string>) (sets:array<Set<'a>>) =
-        createUpsetWith labels sets 15 Color.Table.Office.darkBlue Color.Table.Office.lightBlue (0.,0.2) (0.3,1.)
+        createUpsetWith labels sets 15 Color.Table.Office.darkBlue Color.Table.Office.lightBlue (0.,0.2) (0.3,1.) (Font.init(StyleParam.FontFamily.Arial,Size=20.))
